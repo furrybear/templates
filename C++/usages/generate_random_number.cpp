e287fab413a0d6a812c3c16a84192a0d2ed3d24e
furrrybear <bearcubhaha@gmail.com>
2018-09-09 16:19:33 +0800

    Some change.

---
+++ C++/usages/generate_random_number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+int main()
+{
+    //随机数引擎
+    default_random_engine e;
+    //随机浮点数分布类对象，范围为[0,9]
+    uniform_real_distribution<double> rr(0.0, 1.0);
+    for (int i = 0; i < 10; ++i)
+        cout << rr(e) << endl;
+    //随机非负整数分布类对象，范围为[0,9]
+    uniform_int_distribution<unsigned int> ri(0, 9);
+    for (int i = 0; i < 10; ++i)
+        cout << ri(e) << endl;
+    return 0;
+}
