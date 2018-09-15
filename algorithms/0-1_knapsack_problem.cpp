2ba5d0c386aa7b5b8b587d417c72c3c1ad4ec13c
furrybear <lfbll@outlook.com>
2018-09-15 08:59:25 +0800

    Some change.

--- algorithms/0-1_knapsack_problem.cpp
+++ algorithms/0-1_knapsack_problem.cpp
@@ -1,22 +1,24 @@
+/*******************************************
+Problem: 0-1 knapsack problem.
+Author: furrybear<bearcubhaha@gmail.com>
+Input Sample：
+4
+20 20
+50 30
+100 60
+5 3
+
+Output Sample：
+94
+********************************************/
 #include <array>
 #include <iostream>
 #include <vector>
 using namespace std;
-/*
-输入：
-4
-20 20 100 60
-50 30 80 55
-100 60 110 88
-5 3 10 6
-输出：
-94
-*/
-array<array<int, 121>, 2> dp_cache; //dp_cache[第i个题目][已经使用的时间]，值为分数
-vector<int> t1; //时间
-vector<int> s1; //分数
-vector<int> t2; //时间
-vector<int> s2; //分数
+
+array<array<int, 121>, 2> dp_cache; //dp_cache[i-th thing][volume used], its value implies weight
+vector<int> t1; //Every volume
+vector<int> s1; //Every weight
 
 int main()
 {
@@ -30,11 +32,9 @@ int main()
         cin >> a >> b >> c >> d;
         t1.push_back(a);
         s1.push_back(b);
-        t2.push_back(c);
-        s2.push_back(d);
     }
 
-    for (n = 1; n <= N; n++) { //n表示当前要处理第几道题
+    for (n = 1; n <= N; n++) { //n implies n-th thing
         int iBase = !(n % 2);
         int iToCalculate = n % 2;
         for (int t = 0; t <= 120; t++) {
@@ -42,8 +42,6 @@ int main()
             tmp = max(dp_cache[iBase][t], tmp);
             if (t >= t1[n - 1])
                 tmp = max(dp_cache[iBase][t - t1[n - 1]] + s1[n - 1], tmp);
-            if (t >= t2[n - 1])
-                tmp = max(dp_cache[iBase][t - t2[n - 1]] + s2[n - 1], tmp);
             dp_cache[iToCalculate][t] = tmp;
         }
         for (int i = 0; i < dp_cache[iToCalculate].size(); i++) {
